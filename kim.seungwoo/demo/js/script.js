522b607c843fd2400b22d892642324e39e605750
Seungwoo Kim <seungwookim@Seungwoos-MacBook-Pro.local>
2021-09-29 20:11:56 -0700

    inclass update

---
+++ kim.seungwoo/demo/js/script.js
@@ -0,0 +1,23 @@
+
+$(()=>{
+	$(".accordion dt").on("click", function(e){
+		// lexical this
+
+		// $(this).next().slideToggle();
+
+		$(this)
+			.next().slideDown()
+			.siblings("dd").slideUp();
+
+	});
+
+	$(".tabgroup .tab").on("click",function(e){
+		let index = $(this).index();
+
+		$(this).addClass("active")
+			.siblings().removeClass("active");
+		$(this).closest(".tabgroup")
+			.find(".content").eq(index).addClass("active")
+			.siblings().removeClass("active");
+	})
+});
